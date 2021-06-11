761a39ca25311211658422c57deabcebcf7c814d
Luukvdm <luuk.vdm@hotmail.com>
2021-06-11 12:42:13 +0200

    Prod db (#27)
    * Dockerfile and function in migrate file
    
    * Prod db setup with migrations

--- src/Services/Identity/Identity.Api/Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/Services/Identity/Identity.Api/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,4 +1,3 @@
-using System.Reflection;
 using Kwetter.Services.Identity.Api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
--- src/scripts/migrate.sh
+++ src/scripts/migrate.sh
@@ -1,3 +1,43 @@
+<<<<<<< HEAD
+export ASPNETCORE_ENVIRONMENT=Production
+
+migrate_tweet() {
+    local name="$1"
+    local proj="$2"
+
+#    dotnet ef migrations add "$name" \
+#	--project $proj/Infrastructure \
+#	--startup-project $proj/Api \
+#	--output-dir Persistence/Migrations
+
+    dotnet ef migrations add "$name" \
+	--project Services/Tweet/Api/Tweet.Infrastructure \
+	--startup-project Services/Tweet/Api/Tweet.Api \
+	--output-dir Persistence/Migrations
+}
+
+migrate_identity() {
+    dotnet ef migrations add "$1" \
+	--context ApplicationDbContext \
+	--project Services/Identity/Identity.Api \
+	--startup-project Services/Identity/Identity.Api \
+	--output-dir Infrastructure/Persistence/Migrations \
+	# --configuration Release
+
+#    dotnet ef migrations add "$1" \
+#	--context ConfigurationDbContext \
+#	--project Services/Identity/Identity.Api \
+#	--startup-project Services/Identity/Identity.Api \
+#	--output-dir Infrastructure/Persistence/Migrations/ConfigurationDb \
+#	# --configuration Release
+
+}
+
+# migrate_identity "init-migration"
+# migrate_tweet "init-migration"
+
+||||||| 40a427a
+=======
 export ASPNETCORE_ENVIRONMENT=Production
 
 migrate_tweet() {
@@ -38,3 +78,4 @@ migrate_identity() {
 # migrate_identity "init-migration"
 # migrate_tweet "init-migration"
 
+>>>>>>> main
