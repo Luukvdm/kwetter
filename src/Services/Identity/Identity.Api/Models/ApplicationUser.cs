48d32dd2d13ea0e4171d17067c837613d464dea9
Luukvdm <luuk.vdm@hotmail.com>
2021-06-11 10:07:47 +0200

    Fixed confusion and bug with user names (#20)

--- src/Services/Identity/Identity.Api/Models/ApplicationUser.cs
+++ src/Services/Identity/Identity.Api/Models/ApplicationUser.cs
@@ -5,7 +5,6 @@ namespace Kwetter.Services.Identity.Api.Models
 {
     public class ApplicationUser : IdentityUser
     {
-       [Required] public string Name { get; set; }
        [Required] public string DisplayName { get; set; }
     }
 }
