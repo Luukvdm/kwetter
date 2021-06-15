9c48e303b7a98292da97ac7ec5e3381184e118a9
Luuk van den Maagdenberg <luuk.vdm@hotmail.com>
2021-06-15 19:41:26 +0200

    Small fixes

--- src/serverless/ContentFilter/ContentFilter/index.ts
+++ src/serverless/ContentFilter/ContentFilter/index.ts
@@ -3,7 +3,7 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import * as rd from 'readline'; */
 import badwords from "./array";
 
-function containsBadWord(text: String) {
+function containsBadWord(context: Context, text: String) {
     // https://github.com/RobertJGabriel/Google-profanity-words
     // Ty corporate overlord <3
 
@@ -12,36 +12,48 @@ function containsBadWord(text: String) {
     reader.on("line", (word: string) => {
         if(text.includes(word)) return true;
     }); */
+    let result = false;
 
-    badwords.forEach(word => {
-        if(text.includes(word)) return true;
+    badwords.every(word => {
+        if(text.indexOf(word) >= 0) {
+            result = true;
+            return false;
+        }
+        return true;
     });
 
-    return false;
+    return result;
 }
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
     const content = (req.query.content || req.body);
 
-    console.log("Received data:");
-    console.log(content);
+    context.log("Received data:");
+    context.log(content);
 
     let response :String;
 
     if(content) {
-        if(containsBadWord(content)) {
+        if(containsBadWord(context, content)) {
             response = "bad";
+            context.log("Word is bad :(");
         } else {
             response = "good";
+            context.log("Word is good :)");
         }
+        context.res = {
+            // 200 is cool
+            status: 200,
+            body: response
+        };
+    } else {
+        context.log("Content bad")
+        context.res = {
+            status: 400,
+            body: "dunno"
+        };
     }
-
-    context.res = {
-        // 200 is cool
-        status: 200,
-        body: response
-    };
 };
 
 export default httpTrigger;
