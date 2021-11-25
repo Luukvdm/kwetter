437b6cb758ef12ec54d6f27da43db4383c704735
Luuk van den Maagdenberg <luuk.vdm@hotmail.com>
2021-11-25 15:40:56 +0100

    Removed serverless content filter

--- src/Services/Tweet/Api/Tweet.Application/Commands/CreateTweetMessage/CreateTweetMessageValidator.cs
+++ src/Services/Tweet/Api/Tweet.Application/Commands/CreateTweetMessage/CreateTweetMessageValidator.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Net.Http;
-using System.Text;
 using FluentValidation;
 using Kwetter.BuildingBlocks.Abstractions.Services;
 using Kwetter.Services.Tweet.Domain.Entities;
@@ -16,20 +14,6 @@ namespace Kwetter.Services.Tweet.Application.Commands.CreateTweetMessage
                 .WithMessage($"Message must not be empty and less then {TweetMessage.MAX_MESSAGE_LENGTH} characters.");
             RuleFor(e => e.PosterId).NotEmpty();
             RuleFor(e => e.PostTime).NotEmpty().LessThan(dateTime.Now).WithMessage("Can't tweet in the future now.");
-            RuleFor(e => e.Message).MustAsync(async (command, message, cancellationToken) =>
-            {
-                var enabledSection = configuration.GetSection("ContentChecker:Enabled");
-                if (!enabledSection.Exists() && enabledSection.Value.ToLower() != "true") return true;
-                string url = configuration["ContentChecker:Url"];
-                var client = new HttpClient
-                {
-                    Timeout = TimeSpan.FromSeconds(5)
-                };
-                var result = await client.PostAsync(url, new StringContent(message, Encoding.UTF8));
-                string content = await result.Content.ReadAsStringAsync();
-                return content is "good" or "dunno";
-
-            }).WithMessage("Your tweet contains bad language");
         }
     }
 }
