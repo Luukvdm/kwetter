c8b2752732a7518b85807f533d01e360de400e19
Luuk van den Maagdenberg <luuk.vdm@hotmail.com>
2021-12-02 00:17:54 +0100

    Moved deploy files

--- deploy/k8s/deploy.sh
+++ deploy/k8s/deploy.sh
@@ -2,6 +2,8 @@
 
 NAMESPACE="kwetter"
 
+kubectl --namespace=$NAMESPACE apply -f base.yaml | grep -v "unchanged"
+
 for f in ./*.yaml
 do
 	kubectl --namespace=$NAMESPACE apply -f $f | grep -v "unchanged"
